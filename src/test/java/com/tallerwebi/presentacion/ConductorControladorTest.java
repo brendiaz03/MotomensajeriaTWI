c708c12d3b1e775e13edd3c42c1f450bdfefe773
facuvarela00 <facundo.varela00@gmail.com>
2024-05-21 19:46:45 -0300

    Correciones visuales + Intentos de Test de conductor

--- src/test/java/com/tallerwebi/presentacion/ConductorControladorTest.java
+++ src/test/java/com/tallerwebi/presentacion/ConductorControladorTest.java
@@ -1,51 +1,72 @@
-//package com.tallerwebi.presentacion;
-//
-//import com.tallerwebi.dominio.conductor.Conductor;
-//import com.tallerwebi.dominio.conductor.ConductorDuplicadoException;
-//import com.tallerwebi.dominio.conductor.ConductorNoEncontradoException;
-//import com.tallerwebi.dominio.conductor.ConductorServicio;
-//import com.tallerwebi.dominio.imagen.Imagen;
-//import com.tallerwebi.dominio.imagen.ImagenServicio;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import javax.servlet.http.HttpSession;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.equalToIgnoringCase;
-//import static org.mockito.Mockito.*;
-//
-//public class ConductorControladorTest {
-//
-//   private ConductorControlador conductorControlador;
-//   private ConductorServicio conductorServicio;
-//   private ImagenServicio imagenServicio;
-//   private HttpSession session;
-//
-//
-//    @BeforeEach //antes que ejecuten los test, se ejecute este método (como un constructor de test)
-//   public void init() throws Exception {
-//       this.conductorServicio = mock(ConductorServicio.class);
-//       this.imagenServicio = mock(ImagenServicio.class);
-//       this.session = mock(HttpSession.class);
-//       this.conductorControlador = new ConductorControlador(this.conductorServicio, this.imagenServicio);
-//
-//   }
-//
-//    @Test
-//    public void queAlSolicitarLaPantallaRegistrarmeSeMuestreElFormularioDeRegistroDelConductor() throws ConductorNoEncontradoException {
-//        ModelAndView mav = this.conductorControlador.mostrarRegistroConductor("", session);
-//        assertThat(mav.getViewName(), equalToIgnoringCase("registro-conductor"));
-//    }
-//
-//    @Test
-//    public void queAlSolicitarLaPantallaIrAPerfilSeMuestreElPerfilDelConductor() throws ConductorNoEncontradoException {
-//        ModelAndView mav = this.conductorControlador.irAPerfil(session);
-//        assertThat(mav.getViewName(), equalToIgnoringCase("perfil-conductor"));
-//    }
-//
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.conductor.Conductor;
+import com.tallerwebi.dominio.conductor.ConductorDuplicadoException;
+import com.tallerwebi.dominio.conductor.ConductorNoEncontradoException;
+import com.tallerwebi.dominio.conductor.ConductorServicio;
+import com.tallerwebi.dominio.imagen.Imagen;
+import com.tallerwebi.dominio.imagen.ImagenServicio;
+import com.tallerwebi.dominio.vehiculo.Vehiculo;
+import com.tallerwebi.dominio.vehiculo.VehiculoServicio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.mockito.Mockito.*;
+
+public class ConductorControladorTest {
+
+   private ConductorControlador conductorControlador;
+   private ConductorServicio conductorServicio;
+   private ImagenServicio imagenServicio;
+
+   private VehiculoServicio vehiculoServicio;
+   private HttpSession session;
+
+
+    @BeforeEach //antes que ejecuten los test, se ejecute este método (como un constructor de test)
+   public void init() throws Exception {
+       this.conductorServicio = mock(ConductorServicio.class);
+       this.imagenServicio = mock(ImagenServicio.class);
+       this.vehiculoServicio=mock(VehiculoServicio.class);
+       this.session = mock(HttpSession.class);
+       this.conductorControlador = new ConductorControlador(this.conductorServicio, this.imagenServicio, this.vehiculoServicio);
+
+   }
+
+    @Test
+    public void queAlSolicitarLaPantallaRegistrarmeSeMuestreElFormularioDeRegistroDelConductor() throws ConductorNoEncontradoException {
+        String nombreEsperado= "registro-conductor";
+        Conductor conductor=new Conductor();
+
+        ModelAndView mav = this.conductorControlador.mostrarFormConductor("", session);
+        assertThat(mav.getViewName(), equalToIgnoringCase(nombreEsperado));
+    }
+
+    @Test
+    public void queAlSolicitarLaPantallaIrAPerfilSeMuestreElPerfilDelConductor() throws ConductorNoEncontradoException {
+        String nombreEsperado = "perfil-conductor";
+        Integer idUsuario = 1;
+
+        Conductor conductor = new Conductor();
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(1L);
+        conductor.setVehiculo(vehiculo);
+
+        when(session.getAttribute("IDUSUARIO")).thenReturn(idUsuario);
+        when(conductorServicio.obtenerConductorPorId(idUsuario)).thenReturn(conductor);
+
+        ModelAndView mav = this.conductorControlador.irAPerfil(session);
+
+        assertThat(mav.getViewName(), equalToIgnoringCase(nombreEsperado));
+    }
+
+
 //    @Test
 //    public void queAlSolicitarLaPantallaEditarSeMuestreElFormularioDeEdicionDelConductor() {
 //        String resultado = this.conductorControlador.mostrarEditarConductor(session);
@@ -63,7 +84,7 @@
 //    @Test
 //    public void queSeRedirijaAVehiculoSiElRegistroEsExitoso() throws Exception {
 //        Conductor nuevoConductor = new Conductor();
-//        when(conductorServicio.verificarDatosDeRegistro(nuevoConductor)).thenReturn(true);
+//        when(conductorServicio.registrarConductorNoDuplicado(nuevoConductor)).thenReturn(true);
 //
 //        ModelAndView mav = this.conductorControlador.registrarConductor(nuevoConductor, session);
 //
@@ -73,7 +94,7 @@
 //    @Test
 //    public void queSeMuestreMensajeDeErrorSiElConductorEsDuplicado() throws Exception {
 //        Conductor nuevoConductor = new Conductor();
-//        doThrow(new ConductorDuplicadoException("Conductor duplicado")).when(conductorServicio).verificarDatosDeRegistro(nuevoConductor);
+//        doThrow(new ConductorDuplicadoException("Conductor duplicado")).when(conductorServicio).registrarConductorNoDuplicado(nuevoConductor);
 //
 //        ModelAndView mav = this.conductorControlador.registrarConductor(nuevoConductor, session);
 //
@@ -98,6 +119,6 @@
 //        verify(session, times(1)).setAttribute("isEditForm", false);
 //        assertThat(mav.getViewName(), equalToIgnoringCase("redirect:/perfil"));
 //    }
-//
-//
-//}
+
+
+}
