98880dba829709600341387d98c4619effbadcb9
Brenda Diaz <brendiaaz03@gmail.com>
2024-07-03 02:15:24 -0300

    estilos homeconductor

--- src/main/webapp/resources/core/js/home-conductor.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const item = document.querySelector('.contenedor-filtro');
-    const pregunta = item.querySelector('.pregunta_acordeon');
-
-    pregunta.addEventListener('click', () => {
-        item.classList.toggle('active');
-    });
-});
